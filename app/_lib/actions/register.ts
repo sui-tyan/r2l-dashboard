66a79d07f8bbdb157d1313f9e147a3a0700b43b4
sui-tyan <mallari23adrian@gmail.com>
2024-06-27 21:04:38 +0800

    feat: can now create account

---
+++ app/_lib/actions/register.ts
@@ -0,0 +1,30 @@
+'use server';
+
+import { api } from '@/lib/api';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export async function register(_currentState: unknown, formData: FormData) {
+  try {
+    if (formData.get('password') !== formData.get('confirm_password')) {
+      throw new Error("Password didn't match!", { cause: 'Not Match' });
+    }
+    const AuthRequestResult = await api.post('/account/create-user', formData);
+    cookies().set('token', AuthRequestResult.data.token, {
+      secure: true,
+      maxAge: 60 * 60 * 24 * 7,
+    });
+  } catch (error: any) {
+    if (error) {
+      switch (error.cause) {
+        case 'Not Match':
+          return "Password didn't match!";
+        default:
+          return 'Something went wrong';
+      }
+    }
+    throw error;
+  }
+
+  redirect('/dashboard');
+}
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -5,8 +7,11 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Separator } from '@/components/ui/separator';
+import { useFormState } from 'react-dom';
+import { register } from '../_lib/actions/register';
 
 export default function Signup() {
+  const [errorMessage, createAccount] = useFormState(register, undefined);
   return (
     <div className="w-full lg:grid lg:grid-cols-2 h-screen relative">
       <button className="absolute right-0 m-5 lg:m-10 font-semibold z-10 ">
@@ -39,54 +44,80 @@ export default function Signup() {
               Enter your email to create your account
             </p>
           </div>
-          <div className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="Email Address"
-                required
-              />
+          <form action={createAccount}>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="Email Address"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Password</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Confirm Password</Label>
+                <Input
+                  id="confirm_password"
+                  type="password"
+                  name="confirm_password"
+                  placeholder="Confirm Password"
+                  required
+                />
+              </div>
+              <div>{errorMessage && <p>{errorMessage}</p>}</div>
+              <Button type="submit" className="w-full">
+                Create Account
+              </Button>
+              <div className="flex w-full justify-center text-center items-center">
+                <Separator className="w-1/3" />
+                <p className="text-[11px] text-gray-500 mx-1">
+                  OR CONTINUE WITH
+                </p>
+                <Separator className="w-1/3" />
+              </div>
+              <Button variant="outline" className="w-full">
+                <svg
+                  className="mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 48 48"
+                >
+                  <path
+                    fill="#fbc02d"
+                    d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+                  ></path>
+                  <path
+                    fill="#e53935"
+                    d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+                  ></path>
+                  <path
+                    fill="#4caf50"
+                    d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+                  ></path>
+                  <path
+                    fill="#1565c0"
+                    d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+                  ></path>
+                </svg>
+                Google
+              </Button>
             </div>
-            <Button type="submit" className="w-full">
-              Continue with Email
-            </Button>
-            <div className="flex w-full justify-center text-center items-center">
-              <Separator className="w-1/3" />
-              <p className="text-[11px] text-gray-500 mx-1">OR CONTINUE WITH</p>
-              <Separator className="w-1/3" />
-            </div>
-            <Button variant="outline" className="w-full">
-              <svg
-                className="mr-2"
-                xmlns="http://www.w3.org/2000/svg"
-                x="0px"
-                y="0px"
-                width="20"
-                height="20"
-                viewBox="0 0 48 48"
-              >
-                <path
-                  fill="#fbc02d"
-                  d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
-                ></path>
-                <path
-                  fill="#e53935"
-                  d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
-                ></path>
-                <path
-                  fill="#4caf50"
-                  d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
-                ></path>
-                <path
-                  fill="#1565c0"
-                  d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-                ></path>
-              </svg>
-              Google
-            </Button>
-          </div>
+          </form>
           <div className="text-gray-500 text-center text-sm">
             By clicking continue, you agree to our Terms of Service and Privacy
             Policy
